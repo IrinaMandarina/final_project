dfb509e6f3048fe2173208140a9b2d53ab38cf7a
IrinaMandarina <irina.e.tarasova@gmail.com>
2020-12-10 21:49:17 +0300

    заставка

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ class Hero(pygame.sprite.Sprite):
 
     def draw(self):
         if hero.dx != 0 and timer % 5 == 0:
-            self.n = (self.n+1) % len(self.images)
+            self.n = (self.n + 1) % len(self.images)
         if self.m == -1:
             image = pygame.transform.flip(self.images[self.n], True, False)
             image.set_colorkey((255, 255, 255))
@@ -46,6 +46,34 @@ class Hero(pygame.sprite.Sprite):
             screen.blit(self.images[self.n], (self.x, self.y))
         self.rect = pygame.Rect(self.x, self.y, 79, 100)
         self.time += 1
+
+
+class Button():
+    def __init__(self, screen, x, y, color, name):
+        self.screen = screen
+        self.x = x
+        self.y = y
+        self.name = name
+        f = pygame.font.Font(None, 36)
+        self.w_x, self.w_y = f.size(self.name)
+        self.color = color
+        self.click = False
+
+    def draw(self):
+        rect(self.screen, self.color, (self.x, self.y, self.w_x + 10, self.w_y + 10))
+        f = pygame.font.Font(None, 36)
+        text = f.render(self.name, 1, (0, 0, 0))
+        self.screen.blit(text, (self.x + 5, self.y + 5))
+
+    def hitting(self, x_m, y_m):
+        if self.x <= x_m <= self.x + self.w_x + 10 and self.y <= y_m <= self.y + self.w_y + 10:
+            self.click = True
+
+    def check(self, x_m, y_m):
+        if self.x <= x_m <= self.x + self.w_x + 10 and self.y <= y_m <= self.y + self.w_y + 10:
+            return True
+        else:
+            return False
 
 
 class Platform(pygame.sprite.Sprite):
@@ -71,10 +99,10 @@ class Platform(pygame.sprite.Sprite):
         polygon(self.screen, (80, 180, 89), points)
 
 
-def move(hero, platforms,k):
+def move(hero, platforms, k):
     m = False
     for platform in platforms:
-        if platform.x <= hero.x <= platform.x + platform.l and hero.y + hero.dy + 100 >= platform.y - 15 and hero.y + 100 <= platform.y - 15:
+        if platform.x - 40 <= hero.x <= platform.x + platform.l - 40 and hero.y + hero.dy + 100 >= platform.y - 15 and hero.y + 100 <= platform.y - 15:
             m = True
             hero.y = platform.y - 115
     if hero.y + hero.dy + 100 >= 700 and hero.y + 100 <= 700:
@@ -104,8 +132,14 @@ def move(hero, platforms,k):
         hero.x += hero.dx
     return k
 
+def zastavka():
+    image = pygame.image.load('zastava2.jpg')
+    image = pygame.transform.scale(image, (1000, 700))
+    # image.set_alpha(200)
+    screen.blit(image, (0, 0))
+    play.draw()
 
-FPS = 20
+FPS = 30
 screen = pygame.display.set_mode((1000, 700))
 
 pygame.display.update()
@@ -114,6 +148,13 @@ finished = False
 platforms = []
 timer = 0
 k = False
+buttons=[]
+play = Button(screen, 480, 250, (100, 100, 100), 'Play!')
+buttons.append(play)
+pl = Platform(screen, 0, 250, 500)
+pl1 = Platform(screen, 0, 400, 700)
+platforms.append(pl)
+platforms.append(pl1)
 
 hero = Hero(screen, 0, 35)
 hero.images.append(pygame.transform.scale(pygame.image.load('1hero.png'), (79, 100)))
@@ -124,21 +165,12 @@ hero.images.append(pygame.transform.scale(pygame.image.load('4hero.png'), (79, 1
 while not finished:
     clock.tick(FPS)
 
-    screen.fill((255, 255, 255))
-    image = pygame.image.load('forest1.jpg')
-    image = pygame.transform.scale(image, (1000, 600))
-    image.set_alpha(200)
-    screen.blit(image, (0, 100))
-
-    pl = Platform(screen, 0, 250, 500)
-    platforms.append(pl)
-    k = move(hero, platforms,k)
-    pl.draw()
-    hero.draw()
-
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             finished = True
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            (x_m, y_m) = pygame.mouse.get_pos()
+            play.hitting(x_m, y_m)
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
                 hero.dx = -4
@@ -147,19 +179,33 @@ while not finished:
                 hero.dx = 4
                 hero.m = 1
             if event.key == pygame.K_UP and k:
-                hero.dy = -10
+                hero.dy = -20
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT:
                 hero.dx = 0
             if event.key == pygame.K_RIGHT:
                 hero.dx = 0
 
+    if not play.click:
+        zastavka()
 
-    f = pygame.font.Font(None, 36)
-    text = f.render('Your game time (sec):' + str(round(timer/20, 1)), 1, (180, 0, 0))
-    screen.blit(text, (400, 30))
+    else:
+        screen.fill((255, 255, 255))
+        image = pygame.image.load('forest1.jpg')
+        image = pygame.transform.scale(image, (1000, 600))
+        image.set_alpha(200)
+        screen.blit(image, (0, 100))
 
-    timer += 1
+        k = move(hero, platforms, k)
+        for pl in platforms:
+            pl.draw()
+        hero.draw()
+
+        f = pygame.font.Font(None, 36)
+        text = f.render('Your game time (sec):' + str(round(timer / FPS, 1)), 1, (180, 0, 0))
+        screen.blit(text, (400, 30))
+
+        timer += 1
 
     pygame.display.update()
 
